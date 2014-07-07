e305692d6dddb28e26ba68c21a734dfdb7751699
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-07 13:22:25 -0700

    udp example fix

--- examples/udp_server.lua
+++ examples/udp_server.lua
@@ -1,10 +1,10 @@
 -- UDP server example 
-local udp = require('udp')
+local udp = require('leda.udp')
 
 local server = udp.Server(10000, 'localhost')
 
 server.started = function() print('server started') end
 
-server.data(function(server, from, data)
+server.data = function(server, from, data)
     print(string.format("received %s from %s", data, from))
 end
