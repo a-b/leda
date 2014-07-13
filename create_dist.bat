4c2fbc3647beae28324e21f866c23ccbf83ba8f9
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-13 09:45:27 -0700

    windows updates

--- create_dist.bat
+++ create_dist.bat
@@ -1,11 +1,4 @@
 xcopy lib dist\lib /y /e
-del dist\lib\moonscript
-
-mkdir  dist\lib\moonscript
-
-xcopy deps\moonscript\moonscript dist\lib\moonscript  /y /e
 xcopy deps\luajit\src\jit\* dist\lib 
 xcopy deps\luajit\src\lua51.dll* dist
-xcopy deps\lpeg\lpeg.dll dist\lib
-xcopy deps\lfs\lfs.dll dist\lib
-xcopy obj\leda.exe dist	
+xcopy leda.exe dist	
