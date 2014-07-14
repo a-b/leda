1bfa37781a6b1ead0c13a2356dc57b0902c78341
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-14 11:22:16 -0700

    updating windows distribution

--- create_dist.bat
+++ create_dist.bat
@@ -1,4 +1,11 @@
 xcopy lib dist\lib /y /e
 xcopy deps\luajit\src\jit\* dist\lib 
-xcopy deps\luajit\src\lua51.dll* dist
+xcopy deps\luajit\src\lua51.dll dist
+xcopy deps\luajit\src\lua51.lib dist
+
+xcopy deps\luajit\src\luajit.exe dist				
+
+mkdir dist\include
+xcopy deps\luajit\src\lua.h dist\include 	 
+
 xcopy leda.exe dist	
