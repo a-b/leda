a038eabe95660f89f96978482073305e5e5a0742
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-15 07:04:20 -0700

    dist bat modification

--- create_dist.bat
+++ create_dist.bat
@@ -1,11 +1,13 @@
 xcopy lib dist\lib /y /e
 xcopy deps\luajit\src\jit\* dist\lib 
 xcopy deps\luajit\src\lua51.dll dist
-xcopy deps\luajit\src\lua51.lib dist
 
 xcopy deps\luajit\src\luajit.exe dist				
 
 mkdir dist\include
 xcopy deps\luajit\src\lua.h dist\include 	 
+xcopy deps\luajit\src\luaconf.h dist\include
+xcopy deps\luajit\src\lauxlib.h dist\include 	 
+
 
 xcopy leda.exe dist	
