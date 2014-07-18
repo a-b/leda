eb334299de47b3dcbedbe2d735cd20dc14a9dc96
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-18 09:22:36 -0700

    renaming modules and adding documentation

--- lib/leda/dict.lua
+++ lib/leda/dict.lua
@@ -1,8 +1,16 @@
+--- Key value store shared between all threads
+-- @usage local dict = require('leda.dict')
+-- dict.set('key', 'value')
+-- local value = dict.get('key') 
+-- @module leda.dict
+
 local export = {}
 
 local separator = ":"
-local addValue
 
+--- set key to value
+-- @param key key string
+-- @param value value string
 export.set = function(key, value)
     if __leda.init then return end
     
@@ -12,7 +20,8 @@ export.set = function(key, value)
     __api.dictionarySet(key, value)
 end
 
-
+--- get value 
+-- @param key key string
 export.get = function(key)
     if __leda.init then return end
     
@@ -21,6 +30,8 @@ export.get = function(key)
     return __api.dictionaryGet(key)
 end
 
+--- delete key 
+-- @param key key string
 export.delete = function(key)
     if __leda.init then return end
     
