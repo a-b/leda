09393fde5af097b1c70ad217b4ca6eee500b515e
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-18 08:58:58 -0700

    updating thread persistence to leveldb

--- lib/leda/dictionary.lua
+++ lib/leda/dictionary.lua
@@ -2,101 +2,31 @@ local export = {}
 
 local separator = ":"
 local addValue
-addValue = function(key, value)
-    if type(value) == 'table' then
-        for name, field in pairs(value) do
-            addValue(key .. separator .. name, field)
-        end
-    else
-        __leda.dictionarySet(key, value)        
-    end
-end
 
 export.set = function(key, value)
     if __leda.init then return end
     
-    if not key or not value then return end
+    assert(key, "no key provided")
+    assert(type(value) == 'string', 'can only store strings')
     
-    addValue(key, value)
+    __api.dictionarySet(key, value)
 end
 
-export.getkeys = function(key)
-    if __leda.init then return end
-    
-    if not key then return end
-    
-    return __leda.dictionaryGetKeys(key)
-end
-
-
-local TableParser = class('TableParser')
-function TableParser:initialize(key, keys)
-    self.prefix = key .. separator
-    self.key = key
-    self.keys = keys
-    self.table = {}
-    
-    self:_next()
-end
-
-function TableParser:_next()
-    if not next(self.keys) then 
-        return 
-    end
-    
-    local index, key = next(self.keys)
-    local fullkey = key
-    local value = export.get(fullkey)
-    
-    local s,e = key:find(self.prefix)
-    key = key:sub(e + 1)
-    
-    local parts = key:split(separator)
-    
-    local table = self.table
-    local parent
-    
-    for _, part in ipairs(parts) do
-        if parent then 
-            table[parent] = {}
-            table = table[parent]
-        end
-        parent = part
-    end
-    
-    
-    table[parent] = value
-    self.keys[index] = nil
-    self:_next()
-end
 
 export.get = function(key)
     if __leda.init then return end
     
-    if not key then return end
+    assert(key, "no key provided")
     
-    local value = __leda.dictionaryGet(key)
-    if not value then
-        local keys = export.getkeys(key)
-        if not next(keys) then return end
-        
-        local result = {}
-        
-        local parser = TableParser(key, keys)
-        return parser.table
-    else
-        return value
-    end
+    return __api.dictionaryGet(key)
 end
 
 export.delete = function(key)
-    local keys = export.getkeys(key)
+    if __leda.init then return end
     
-    for _, subkey in ipairs(keys) do
-        __leda.dictionaryRemove(subkey)
-    end
-    
-    __leda.dictionaryRemove(key)
+    assert(key, "no key provided")
+
+    __api.dictionaryRemove(key)
 end
 
 return export
--- lib/leda/init.lua
+++ lib/leda/init.lua
@@ -6,6 +6,3 @@ require 'middleclass'
 -- reference storage global table
 __leda = __leda or {}
 
-for key, value in pairs(__api) do
-    __leda[key] = value
-end 
